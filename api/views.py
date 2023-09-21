06f239cb7d882a8f186406e4d987788706e726b9
tanulo <tanulo@mechwart.local>
2023-09-21 17:34:18 +0200

    masodik ora eleje

---
+++ api/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse, JsonResponse
+from .models import Pizza
+
+# Create your views here.
+
+def hello(request):
+    return HttpResponse("<h1>Hello!</h1>")
+
+def getPizzas(request):
+    pizzas=Pizza.objects.all()
+    
+    li = []
+
+    for pizza in pizzas:
+        li.append(pizza.serializer())
+
+    return JsonResponse({"data": li})
